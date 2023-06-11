f949db7e938f2674642e87ff7c34c3acd8699cda
mohammed sameh <hmoosameh@gmail.com>
2023-06-11 04:04:15 +0200

    initial comet
    Signed-off-by: mohammed sameh <hmoosameh@gmail.com>

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'home screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
